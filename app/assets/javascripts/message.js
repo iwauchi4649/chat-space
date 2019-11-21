3d479dfc8589693c2111f1be2a704c676aebafc4
Syoma Iwauchi <syoma4645@gmail.com>
2019-11-21 17:30:30 +0900

    comit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
   function buildHTML(message){
     var imagehtml = message.image ? `<img class= "lower-message__image" src="${message.image}" >` : "";
     var html = `<div class='chat-body' data-id="${message.id}">
